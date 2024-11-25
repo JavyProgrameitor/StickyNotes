baaa334a3cecf515f660bec0c415f9131bc947ab
JavyProgrameitor <85842248+JavyProgrameitor@users.noreply.github.com>
2024-11-25 01:30:51 +0100

    Sticky notes is a program created to save notes
    Uses indexedDB, Html, js and css

---
+++ src/js/index.js
@@ -0,0 +1,176 @@
+import { DatabaseStickyNotes } from "./indexedDB.js";
+const database = DatabaseStickyNotes.getInstance();
+
+const noteColorInput = document.querySelector("#noteColor");
+const addInput = document.querySelector("#addButton");
+const mainElement = document.querySelector("main");
+
+let counterID = 0;
+let zIndexValue = 1;
+
+openData().then(() => {
+    uploadNotes();
+    console.log("Data uploaded");
+  });
+  
+
+addInput.addEventListener("click", (event) => {
+
+    event.preventDefault();
+    const color = noteColorInput.value;
+    const text = ""
+    const position = { x: 50, y: 50 };
+    const data = { color, text, position };
+    //const id = counterID;
+
+    database.createData(data)
+      .then((id) => { 
+        createNote(id, position, text, color);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+});
+
+
+
+function createNote(id, position, text, color) {
+  let newNote = document.createElement("div");
+  newNote.classList = "note";
+ // newNote.id = "note-" + counterID;
+  newNote.id = `note-${id}`;
+  newNote.style.left = position.x + "px";
+  newNote.style.top = position.y + "px";
+  newNote.style.zIndex = zIndexValue;
+
+  let noteHeader = document.createElement("div");
+  noteHeader.classList = "noteHeader";
+  noteHeader.style.background = color;
+  noteHeader.innerHTML = `<button class="delete" id="${id}">Close</button>`; 
+
+  newNote.appendChild(noteHeader);
+
+  let noteContent = document.createElement("div");
+  noteContent.classList = "noteContent";
+  noteContent.innerHTML = `<textarea name="noteText" id="noteText-${id}">${text}</textarea>`;
+
+  newNote.appendChild(noteContent);
+
+  const textarea = newNote.querySelector(`#noteText-${id}`);
+  textarea.addEventListener("input", (event) => {
+    const updatedText = event.target.value;
+    updateNote(id, { text: updatedText });
+  });
+
+  //noteHeader.style.background = noteColorInput.value;
+  mainElement.appendChild(newNote);
+  counterID++;
+}
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete")) {
+    const noteId = parseInt(event.target.getAttribute("id"));
+    deleteNote(noteId);
+  }
+});
+
+let cursor = {
+  x: null,
+  y: null,
+};
+
+let note = {
+  dom: null,
+  x: null,
+  y: null,
+};
+
+document.addEventListener("mousedown", (event) => {
+  if (event.target.classList.contains("noteHeader")) {
+    cursor = {
+      x: event.clientX,
+      y: event.clientY,
+    };
+
+    let current = event.target.closest(".note");
+
+    note = {
+      dom: current,
+      x: current.getBoundingClientRect().left,
+      y: current.getBoundingClientRect().top,
+    };
+
+    current.style.cursor = "grabbing";
+    current.style.zIndex = zIndexValue;
+    zIndexValue++;
+  }
+});
+
+document.addEventListener("mousemove", (event) => {
+  if (note.dom == null) {
+    return;
+  }
+
+  let currentCursor = {
+    x: event.clientX,
+    y: event.clientY,
+  };
+
+  let distance = {
+    x: currentCursor.x - cursor.x,
+    y: currentCursor.y - cursor.y,
+  };
+
+  note.dom.style.left = note.x + distance.x + "px";
+  note.dom.style.top = note.y + distance.y + "px";
+});
+document.addEventListener("mouseup", (event) => {
+  if (note.dom) {
+    const noteId = parseInt(note.dom.id.split("-")[1]);
+    const newPosition = {
+      x: parseInt(note.dom.style.left, 10),
+      y: parseInt(note.dom.style.top, 10),
+    };
+    updateNote(noteId, { position: newPosition });
+  }
+  note.dom = null;
+  event.target.parentNode.style.cursor = "grab";
+});
+
+async function openData() {
+  await database.open().catch((error) => {
+    console.error("Error open: " + error);
+  });
+}
+
+async function uploadNotes() {
+  try {
+    const notes = await database.readAllData();
+    notes.forEach((note) => {
+      createNote(note.id, note.position, note.text, note.color);
+    });
+  } catch (error) {
+    console.error("Error upload:", error);
+  }
+}
+
+async function updateNote(id, data) {
+  try {
+    console.log(`Actualizando nota con ID: ${id}`, data); // Debug: 
+    await database.updateData(id, data);
+  } catch (error) {
+    console.error("Error update:", error);
+  }
+}
+
+async function deleteNote(id) {
+  try {
+    await database.deleteData(id);
+    const noteElement = document.querySelector(`#note-${id}`);
+    if (noteElement) {
+      noteElement.remove();
+    }
+  } catch (error) {
+    console.error("Error al eliminar la nota:", error);
+  }
+}
---
+++ src/js/indexedDB.js
@@ -0,0 +1,168 @@
+import { INDEXDB_NAME, INDEXDB_VERSION, STORE_NAME } from "./constants.js";
+
+export class DatabaseStickyNotes {
+
+  constructor(databaseName, databaseVersion) {
+    this.databaseName = databaseName;
+    this.databaseVersion = databaseVersion;
+    this.db = null;
+  }
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new DatabaseStickyNotes(INDEXDB_NAME, INDEXDB_VERSION);
+    }
+    return this.instance;
+  }
+
+  open() {
+    return new Promise((resolve, reject) => {
+      let request = indexedDB.open(this.databaseName, this.databaseVersion);
+      request.onsuccess = (event) => {
+        this.db = event.target.result;
+        resolve();
+      };
+
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
+
+      request.onupgradeneeded = (event) => {
+        let db = event.target.result;
+        if (!db.objectStoreNames.contains(STORE_NAME)) {
+          const objectStore = db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
+
+
+        }
+      };
+
+    });
+  }
+
+  createData(data) {
+    if (!this.db) {
+      throw new Error("La base de datos no está abierta.");
+    }
+
+    return new Promise((resolve, reject) => {
+      let transaction = this.db.transaction([STORE_NAME], "readwrite");
+      let objectStore = transaction.objectStore(STORE_NAME);
+      let request = objectStore.add(data);
+      request.onsuccess = (event) => {
+            // Aquí capturamos el ID asignado por IndexedDB
+      const id = event.target.result;
+      resolve(id); // Devolvemos el ID al resolver la promesa
+      };
+
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
+    });
+  }
+
+  readData(id) {
+    if (!this.db) {
+      throw new Error("La base de datos no está abierta.");
+    }
+
+    return new Promise((resolve, reject) => {
+      let transaction = this.db.transaction([STORE_NAME], "readonly");
+      let objectStore = transaction.objectStore(STORE_NAME);
+      let request = objectStore.get(id);
+
+      request.onsuccess = (event) => {
+        let data = event.target.result;
+        if (data) {
+          resolve(data);
+        } else {
+          reject(new Error("El objeto con el id: " + id + ", no se encontró en la base de datos."));
+        }
+      };
+
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
+    });
+  }
+
+  readAllData() {
+    if (!this.db) {
+      throw new Error("La base de datos no está abierta.");
+    }
+
+    return new Promise((resolve, reject) => {
+      let transaction = this.db.transaction(STORE_NAME);
+      let objectStore = transaction.objectStore(STORE_NAME);
+      let request = objectStore.getAll();
+
+      request.onsuccess = (event) => {
+        let data = event.target.result;
+        if (data) {
+          resolve(data);
+        } else {
+          reject(new Error("Error al obtener todos los datos"));
+        }
+      };
+
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
+    });
+  }
+
+  updateData(id, newData) {
+    if (!this.db) {
+      throw new Error("La base de datos no está abierta.");
+    }
+
+    return new Promise((resolve, reject) => {
+      let transaction = this.db.transaction([STORE_NAME], "readwrite");
+      let objectStore = transaction.objectStore(STORE_NAME);
+      let getRequest = objectStore.get(id);
+
+      getRequest.onsuccess = (event) => {
+        const existingData = event.target.result;
+        if (existingData) {
+          let updatedData = { ...existingData, ...newData };
+          let updateRequest = objectStore.put(updatedData);
+
+          updateRequest.onsuccess = () => {
+            resolve();
+          };
+
+          updateRequest.onerror = (event) => {
+            reject(event.target.error);
+          };
+        } else {
+          reject(new Error("El objeto con el ID especificado no se encontró en la base de datos."));
+        }
+      };
+
+      getRequest.onerror = (event) => {
+        reject(event.target.error);
+      };
+    });
+
+  }
+
+  deleteData(id) {
+    if (!this.db) {
+      throw new Error("La base de datos no está abierta.");
+    }
+
+    return new Promise((resolve, reject) => {
+      let transaction = this.db.transaction([STORE_NAME], "readwrite");
+      let objectStore = transaction.objectStore(STORE_NAME);
+      let request = objectStore.delete(id);
+
+      request.onsuccess = (event) => {
+        resolve();
+      };
+
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
+    });
+  }
+
+}
